e60aa35889f239b3cd7bc30e6e4dead8bc8816d8
Pulkith Paruchuri <ballistic.minecrafter@gmail.com>
2021-04-25 21:21:49 -0500

    add problems -d

---
+++ Kickstart/2020/C/A.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <sstream>
+#include <cmath>
+#include <cstdio>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <queue>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <regex>
+#include <random>
+#include <initializer_list>
+#include <ios>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define ull unsigned long long
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vb = vector<bool>
+#define pb push_back
+
+#define f first
+#define s second
+
+#define nl << "\n";
+
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define ts(x) to_string(x)
+#define rall(x) x.rbegin(), x.rend()
+#define sortt(x) sort(all(x))
+#define rtn return
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define TC(i) for(int tt = (1); tt <= (i); ++tt)
+#define FORE(i, a, b) for(int i = (a); i<= (b); ++i)
+
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+#define lower(sl) transform(sl.begin(), sl.end(), sl.begin(), ::tolower)
+
+const int INF = 1000000007;
+const int MOD = 1000000007;
+const int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
+
+int main () {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    TC(T){
+        int N, K;
+        cin >> N >> K;
+        vi a(N);
+        FOR(i, 0, N)
+            cin >> a[i];
+        int cnt = 0;
+        bool ok = (a[0] == K);
+        FOR(i, 1, N) {
+            if(a[i] == K) 
+                ok = true;
+            else if(!(a[i]+1 == a[i-1])) 
+                ok = false; 
+            if(a[i] == 1 && ok){
+                ++cnt;
+                ok = false;
+            } 
+        }
+        cout << "Case #" << tt << ": " << cnt nl
+    }
+}
